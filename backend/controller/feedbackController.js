f0d1c90218e0585cb7e8221feb7ca654cb7c5fb2
SuchitraShankar07 <suchitrashankar07@gmail.com>
2024-11-17 21:06:56 +0530

    fix pt 9

--- backend/controller/feedbackController.js
+++ backend/controller/feedbackController.js
@@ -5,7 +5,8 @@ exports.postFeedback = async (req, res) => {
      
         const data = await Feedback.create(req.body);
         res.status(200).json(data);
-    } catch (err) 
+    } catch (err) {
+        // Handle any error that occurs
         res.status(400).json({ error: err.message });
     }
 };
